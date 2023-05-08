7fbaa3259f7492af09ab7216b4e5de8313133e41
samuelgordon <samuelgordon@gmail.com>
2023-05-08 21:36:50 +0000

    Changed the role of the assistant to make it give incorrect responses

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import openai
 openai.api_key = os.getenv("OpenAI API Key")
 
 while True:
-  question = input("\033[34mWhat can I answer for you?\n\033[0m")
+  question = input("\033[34mWhat can I answer incorrectly for you?\n\033[0m")
 
   if question.lower() == "exit":
     print("\033[31mGoodbye!\n\033[0m")
@@ -13,7 +13,7 @@ while True:
   completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-      {"role": "system", "content": "You are an assistant. Answer the given question."},
+      {"role": "system", "content": "You are an assistant. Answer the given question with a clearly incorrect answer."},
       {"role": "user", "content": question}
   ]
   )
